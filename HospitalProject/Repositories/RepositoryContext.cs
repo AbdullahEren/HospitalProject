e42bce711b589c8cb44fe7d6d900a17b5c0784e6
AbdullahEren <a.eyurek@hotmail.com>
2023-11-21 14:04:06 +0300

    Implementing RepositoryContext.cs

---
+++ HospitalProject/Repositories/RepositoryContext.cs
@@ -0,0 +1,30 @@
+﻿using HospitalProject.Entities.Models;
+using HospitalProject.Repositories.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalProject.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<FamilyDoctorChange> FamilyDoctorChanges { get; set; }
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new DoctorConfig());
+            modelBuilder.ApplyConfiguration(new PatientConfig());
+            modelBuilder.ApplyConfiguration(new MedicineConfig());
+            modelBuilder.ApplyConfiguration(new AppointmentConfig());
+            modelBuilder.ApplyConfiguration(new PrescriptionConfig());
+            modelBuilder.ApplyConfiguration(new FamilyDoctorChangeConfig());
+
+        }
+    }
+}
