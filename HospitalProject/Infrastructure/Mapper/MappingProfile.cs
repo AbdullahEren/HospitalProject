eb8ff2c7c1072b3af9a7fe1afb3037fab0eac5c6
AbdullahEren <a.eyurek@hotmail.com>
2023-11-21 21:16:28 +0300

    Implementing AutoMapper to Project

---
+++ HospitalProject/Infrastructure/Mapper/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HospitalProject.Entities.Dtos;
+using HospitalProject.Entities.Models;
+
+namespace HospitalProject.Infrastructure.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<PatientDtoForCreation, Patient>();
+            CreateMap<PatientDtoForUpdate,Patient>();
+            CreateMap<Patient,PatientDtoForRead>().ReverseMap();
+            CreateMap<DoctorDtoForCreation, Doctor>();
+            CreateMap<DoctorDtoForUpdate,Doctor>();
+            CreateMap<Doctor,DoctorDtoForRead>().ReverseMap();
+            CreateMap<MedicineDtoForCreation, Medicine>();
+            CreateMap<MedicineDtoForUpdate,Medicine>();
+            CreateMap<Medicine,MedicineDtoForRead>().ReverseMap();
+            CreateMap<PrescriptionDtoForCreation, Prescription>();
+            CreateMap<Prescription,PrescriptionDtoForRead>().ReverseMap();
+            CreateMap<AppointmentDtoForCreation,Appointment>();
+            CreateMap<AppointmentDtoForUpdate,Appointment>();
+            CreateMap<Appointment,AppointmentDtoForRead>().ReverseMap();
+
+        }
+    }
+}
